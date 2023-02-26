2be62ec401055367648cbb31acc35f380cc39243
Carefree-happy <2507057523@qq.com>
2023-02-27 02:26:52 +0800

    3.6 如何自定义错误信息

--- schema-tests/test1.js
+++ schema-tests/test1.js
@@ -7,8 +7,10 @@ const schema = {
     properties: {
         name: {
             type: "string",
-            test: true,
+            // test: true,
             // format: "email",
+            minLength: 12,
+            errorMessage: "aas",
         },
         age: {
             type: "number",
@@ -38,7 +40,7 @@ const data = {
     words: "hahah",
 }
 
-var ajv = new Ajv()
+var ajv = new Ajv({ allErrors: true })
 // 添加 format
 addFormats(ajv)
 
@@ -47,14 +49,16 @@ addFormats(ajv)
 //     return data === "haha"
 // })
 
+require("ajv-errors")(ajv)
+
 ajv.addKeyword("test", {
     // 课程的方式，不能通过 these parameters are deprecated, see docs for addKeyword
     // 返回一个 schema
-    // macro() {
-    //     return {
-    //         minLength: 12,
-    //     }
-    // },
+    macro() {
+        return {
+            minLength: 12,
+        }
+    },
     // 课程的方式，不能通过 these parameters are deprecated, see docs for addKeyword
     // // 数据校验时调用，sch 对应关键字的值，parentSchema 对应 test 所在属性的定义
     // compile(sch, parentSchema) {
@@ -65,25 +69,25 @@ ajv.addKeyword("test", {
     // metaSchema: {
     //     type: "boolean",
     // },
-    validate: function fun(schema, data) {
-        // 数据校验时调用，schema 对应关键字的值， data 对应实际的值，return 进行错误信息自定义
-        // 添加自定义关键字, 自定义返回的错误信息，键对值不对
-        fun.errors = [
-            {
-                instancePath: "/name",
-                schemaPath: "#/properties/name/test",
-                keyword: "test",
-                params: { keyword: "test" },
-                message: '应当通过 "test 关键词"',
-            },
-        ]
-        return false
-    },
+    // validate: function fun(schema, data) {
+    //     // 数据校验时调用，schema 对应关键字的值， data 对应实际的值，return 进行错误信息自定义
+    //     // 添加自定义关键字, 自定义返回的错误信息，键对值不对
+    //     fun.errors = [
+    //         {
+    //             instancePath: "/name",
+    //             schemaPath: "#/properties/name/test",
+    //             keyword: "test",
+    //             params: { keyword: "test" },
+    //             message: '应当通过 "test 关键词"',
+    //         },
+    //     ]
+    //     return false
+    // },
 })
 
 var validate = ajv.compile(schema)
 var valid = validate(data)
 if (!valid) {
-    locatize.zh(validate.errors)
+    // locatize.zh(validate.errors)
     console.log(validate.errors)
 }
