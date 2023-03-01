afcf876532998c3d544270de23ca89050a58dd7b
Carefree-happy <2507057523@qq.com>
2023-03-01 14:50:07 +0800

    ci: test:unit

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,13 +1,24 @@
+import { defineComponent, h } from "vue"
 import { shallowMount } from "@vue/test-utils"
-// import HelloWorld from "@/components/HelloWorld.vue"
 
-// describe("HelloWorld.vue", () => {
-//     it("renders props.msg when passed", () => {
-//         const msg = "new message"
-//         const age = 12
-//         const wrapper = shallowMount(HelloWorld, {
-//             props: { msg, age },
-//         })
-//         expect(wrapper.text()).toMatch(msg)
-//     })
-// })
+const HelloWorld = defineComponent({
+    name: "HelloWorld",
+    props: {
+        msg: String,
+    },
+    setup(props) {
+        return () => {
+            return h("div", props.msg)
+        }
+    },
+})
+
+describe("HelloWorld.vue", () => {
+    it("renders props.msg when passed", () => {
+        const msg = "new message"
+        const wrapper = shallowMount(HelloWorld, {
+            props: { msg },
+        })
+        expect(wrapper.text()).toMatch(msg)
+    })
+})
