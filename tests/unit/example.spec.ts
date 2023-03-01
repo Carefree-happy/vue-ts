04aac7475326ab9df34a3996e251588d77cd5b94
Carefree-happy <2507057523@qq.com>
2023-03-01 15:48:13 +0800

    ci: 6.2 6.3 如何使用jest写测试用例

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -13,12 +13,66 @@ const HelloWorld = defineComponent({
     },
 })
 
+beforeEach(() => {
+    console.log("before each")
+})
+
+afterEach(() => {
+    console.log("after each")
+})
+
+beforeAll(() => {
+    console.log("before all")
+})
+
+afterAll(() => {
+    console.log("after all")
+})
+
 describe("HelloWorld.vue", () => {
-    it("renders props.msg when passed", () => {
+    beforeEach(() => {
+        console.log("before each2")
+    })
+
+    afterEach(() => {
+        console.log("after each2")
+    })
+
+    beforeAll(() => {
+        console.log("before all2")
+    })
+
+    afterAll(() => {
+        console.log("after all2")
+    })
+    it("renders props.msg when passed", async () => {
         const msg = "new message"
+        console.log("render")
         const wrapper = shallowMount(HelloWorld, {
             props: { msg },
         })
-        expect(wrapper.text()).toMatch(msg)
+        // 前面传入 done
+        // setTimeout(() => {
+        //     expect(wrapper.text()).toMatch(msg)
+        //     done()
+        // }, 100)
+        // 或者 promise
+        // return new Promise<void>((resolve) => {
+        //     // "123" 不通过
+        //     expect(wrapper.text()).toMatch(msg)
+        //     resolve()
+        // })
+        // 或者 async/await
+        await wrapper.setProps({
+            msg: "123",
+        })
+        expect(wrapper.text()).toMatch("123")
+    })
+})
+
+describe("another", () => {
+    it("should work", () => {
+        expect(1 + 1).toBe(2)
+        console.log("another")
     })
 })
